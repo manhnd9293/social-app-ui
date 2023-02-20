fb55b7958249183b66ad0233e55990476bd730fb
Manh Nguyen <manhnd9293@gmail.com>
2023-02-20 15:00:13 +0700

    change header

--- src/layout/header/Header.js
+++ src/layout/header/Header.js
@@ -6,7 +6,7 @@ function Header() {
     <nav className="navbar is-primary" role="navigation" aria-label="main navigation">
       <div className="navbar-brand">
         <Link className="navbar-item" to="/">
-          <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28"/>
+         <span>HuNi</span>
         </Link>
 
         <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false"
