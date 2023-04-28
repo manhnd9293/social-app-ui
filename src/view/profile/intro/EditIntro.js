bc152ce77d6f88478e70b0f1f639844471a83fd2
Manh Nguyen <manhnd9293@gmail.com>
2023-04-28 22:29:42 +0700

    H53 add show property for intro data

--- src/view/profile/intro/EditIntro.js
+++ src/view/profile/intro/EditIntro.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import BaseModal from "../../../common/modal/BaseModal";
 
 function EditIntro({onClose}) {
+
+  const [educations, setEducations] = useState(currentUser.educations);
+  const [relationship, setRelationship] = useState(currentUser.relationship);
+
+  function toggleShowEducation(index) {
+    const updateEducations = structuredClone(educations);
+    updateEducations[index].show = !educations[index].show;
+    setEducations(updateEducations);
+  }
+
+  function toggleRelationship() {
+    const update = structuredClone(relationship);
+    update.show = !relationship.show;
+    setRelationship(update);
+  }
+
   return (
     <BaseModal modalName={`Edit details`}
                closeModal={onClose}
@@ -24,17 +40,30 @@ function EditIntro({onClose}) {
           <div className={`has-text-weight-bold`}>Education</div>
           <div className={`mt-3`}>
             <div>
-              <div className="field">
-                <input id="switchRoundedInfo" type="checkbox" name="switchRoundedInfo"
-                       className="switch is-rounded is-info" checked="checked"
-                       onChange={() => {
+              {
+                educations.map((edu, index) =>
+                  <div key={edu.name} className={`is-flex is-justify-content-space-between`}>
+                    <div>
+                      <div className="field">
+                        <input id={`switchEdu-${index}`}
+                               type="checkbox"
+                               name="switchRoundedInfo"
+                               className="switch is-rounded is-info"
+                               checked={edu.show}
+                               onChange={() => toggleShowEducation(index)}
+                        />
+                        <label htmlFor={`switchEdu-${index}`} style={{userSelect: 'none'}}>
+                          {`Studying at ${edu.name}`}
+                        </label>
+                      </div>
 
-                       }}
-                />
-                  <label htmlFor="switchRoundedInfo">
-                    Studied at Foreign Trade University
-                  </label>
-              </div>
+                    </div>
+                    <div className={`icon is-clickable`}>
+                      <i className="fa-solid fa-pen"></i>
+                    </div>
+                  </div>
+                )
+              }
             </div>
           </div>
           <div className={`button is-outlined is-small is-info is-rounded mt-3`}>
@@ -53,16 +82,70 @@ function EditIntro({onClose}) {
         </div>
         <div className={`mt-3`}>
           <div className={`has-text-weight-bold`}>Current city</div>
+          <AddDataBtn name={'Add City'}/>
         </div>
         <div className={`mt-3`}>
           <div className={`has-text-weight-bold`}>Home town</div>
+          <AddDataBtn name={'Add hometown'}/>
         </div>
         <div className={`mt-3`}>
           <div className={`has-text-weight-bold`}>Relationship</div>
+          <div className={`is-flex is-justify-content-space-between`}>
+            <div>
+              <div className="field">
+                <input id={`switchRel`}
+                       type="checkbox"
+                       name="switchRoundedInfo"
+                       className="switch is-rounded is-info"
+                       checked={relationship.show}
+                       onChange={toggleRelationship}
+                />
+                <label htmlFor={`switchRel`} style={{userSelect: 'none'}}>
+                  {`${relationship.state}`}
+                </label>
+              </div>
+
+            </div>
+            <div className={`icon is-clickable`}>
+              <i className="fa-solid fa-pen"></i>
+            </div>
+          </div>
         </div>
       </div>
     </BaseModal>
   );
 }
 
+function AddDataBtn({name}) {
+  return (
+    <div className={`button is-outlined is-small is-info is-rounded mt-3`}>
+            <span className={`icon`}>
+              <i className="fa-solid fa-plus"></i>
+            </span>
+      <span>{name}</span>
+    </div>
+  )
+}
+
 export default EditIntro;
+const currentUser = {
+  educations: [
+    {
+      show: true,
+      name: 'Foreign Trade University',
+      level: 'University',
+      state: 'Learning'
+    },
+    {
+      show: false,
+      name: 'HNUE High school for gifted student',
+      level: 'High School',
+      state: 'Graduate'
+    }
+  ],
+  relationship: {
+    state: 'Single',
+    show: 'true'
+  }
+}
+
