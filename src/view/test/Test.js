5ab754033e1b337bdb149937556edf6a4dc9959b
Manh Nguyen <manhnd9293@gmail.com>
2023-05-21 21:40:21 +0700

    example view update/create gw

--- src/view/test/Test.js
+++ src/view/test/Test.js
@@ -1,64 +1,82 @@
-import React, {useEffect} from 'react';
-import {beClient} from "../../config/BeClient";
-import {useInfiniteQuery} from "react-query";
-
-async function loadTests({pageParam = 0}) {
-  return beClient.get(`/test?lastId=${pageParam}`);
-}
+import React, { useState } from 'react';
 
 function Test() {
+  const [li, setLi] = useState('');
+  const [showConfirm, setShowConfirm] = useState(false);
+  const [data, setData] = useState('');
+  const [id, setId] = useState('');
 
-  const {
-    data,
-    error,
-    fetchNextPage,
-    hasNextPage,
-    isFetching,
-    isFetchingNextPage,
-    status
-  } = useInfiniteQuery({
-    queryKey: ['tests'],
-    queryFn: loadTests,
-    getNextPageParam: (lastPage, pages) => {
-      if(!lastPage.hasNext) return false;
-
-      const lastId = lastPage.items[lastPage.items.length -1];
-      // debugger
-      return lastId;
+  function submit() {
+    if(!li || id){
+      updateGw();
+    } else if(!id) {
+      setShowConfirm(true);
     }
-  });
+  }
 
-  useEffect(() => {
-    console.log('load new page');
-  }, [data?.pages?.length])
+  function updateGw() {
+    if (!li || id) {
+      console.log({data, id: id || null});
+    } else {
+      console.log({data, li});
+    }
+  }
 
-  return status === 'loading' ? (
-    <p>Loading...</p>
-  ) : status === 'error' ? (
-    <p>Error: {error.message}</p>
-  ) : (
-    <>
-      {data.pages.map((group, i) => (
-        <React.Fragment key={i}>
-          {group.items.map((item) => (
-            <p key={item}>{item}</p>
-          ))}
-        </React.Fragment>
-      ))}
+  return (
+    <div>
+      <div>hello test view</div>
       <div>
-        <button
-          onClick={() => fetchNextPage()}
-          disabled={!hasNextPage || isFetchingNextPage}
-        >
-          {isFetchingNextPage
-            ? 'Loading more...'
-            : hasNextPage
-              ? 'Load More'
-              : 'Nothing more to load'}
-        </button>
+        <div>license</div>
+        <div>
+          <input type={`text`}
+                 value={li}
+                 onChange={e => setLi(e.target.value)}
+          />
+        </div>
+
+        <div className={`mt-3`}>
+          <div>
+            <div>ID</div>
+            <div>
+              <input type={"text"}
+                     value={id}
+                     onChange={e => setId(e.target.value)}
+              />
+            </div>
+          </div>
+          <div>
+            <div>Data</div>
+            <div>
+              <input type={"text"}
+                     value={data}
+                     onChange={e => setData(e.target.value)}
+              />
+            </div>
+          </div>
+          <button onClick={submit}
+                  className={`mt-3`}
+          >
+            Create gw
+          </button>
+        </div>
       </div>
-      <div>{isFetching && !isFetchingNextPage ? 'Fetching...' : null}</div>
-    </>)
+      {
+        showConfirm &&
+        <div className={`mt-3`}>
+          <div>Do you want to add license</div>
+          <div>
+            <button onClick={updateGw}>Yes</button>
+            <button className={`ml-3`}
+                    onClick={() => setShowConfirm(false)}
+            >
+              No
+            </button>
+          </div>
+        </div>
+      }
+
+    </div>
+  );
 }
 
 
